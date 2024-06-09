a7fbcc39e3a98c380fc9cd8f39aa49348b6e4c0a
Bugaev Kirill <esyjeteg17@gmail.com>
2024-06-09 14:45:22 +0300

    ready project on fake api

---
+++ components/TABS.ts
@@ -0,0 +1,13 @@
+export interface Tab {
+	title: string
+	tab: string
+}
+
+export const tabs: Tab[] = [
+	{ title: 'Мобильная связь', tab: 'mobile' },
+	{ title: 'Для модемов и роутеров', tab: 'modems' },
+	{ title: 'Домашний телефон', tab: 'homephone' },
+	{ title: 'Спутниковое ТВ', tab: 'tv' },
+	{ title: 'Домашний интернет', tab: 'internet' },
+	{ title: 'Для умных устройств', tab: 'smart' },
+]
---
+++ layouts/default.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <Header />
+    <div class="w-10/12 mx-auto">
+      <slot />
+    </div>
+    <!-- <Footer /> -->
+  </div>
+</template>
