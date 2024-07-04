4e8848a8f4928b5aaf3a170b92b30c4e71570175
Kirill Bugaev <esyjeteg17@gmail.com>
2024-07-04 09:48:35 +0300

    First

--- layouts/default.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div>
-    <Header />
-    <div class="w-10/12 mx-auto">
-      <slot />
-    </div>
-    <!-- <Footer /> -->
-  </div>
-</template>
--- pages/about/index.vue
+++
@@ -1,3 +0,0 @@
-<template>
-  <h1 class="text-3xl text-center mt-10">About page</h1>
-</template>
